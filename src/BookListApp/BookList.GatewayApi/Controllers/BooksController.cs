46bf0cc0f25d6a9b875687a00e6176c704f18899
Brandon Sharp <brandon.sharp@outlook.com>
2016-10-07 15:01:12 -0500

    Added some helper methods

--- src/BookListApp/BookList.GatewayApi/Controllers/BooksController.cs
+++ src/BookListApp/BookList.GatewayApi/Controllers/BooksController.cs
@@ -4,11 +4,16 @@ using BookList.GatewayApi.Models;
 using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Client;
 using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -18,6 +23,13 @@ namespace BookList.GatewayApi.Controllers {
     public class BooksController : ApiController {
         readonly string libraryServiceName = "Library";
         readonly string bookActorServiceName = "BookActorService";
+
+        private readonly HttpCommunicationClientFactory clientFactory = new HttpCommunicationClientFactory(
+                ServicePartitionResolver.GetDefault(),
+                "EndpointName",
+                TimeSpan.FromSeconds(10),
+                TimeSpan.FromSeconds(3));
+
 
         [HttpPost]
         [Route("{isbn}")]
@@ -68,6 +80,58 @@ namespace BookList.GatewayApi.Controllers {
                 () => new HttpRequestMessage(HttpMethod.Get, route));
 
             return response;
+        }
+
+        async Task<bool> PostDataToService<T>(string serviceName, ServicePartitionKey partitionKey, string route, T payload) {
+            var serviceUri = new ServiceUriBuilder(serviceName).ToUri();
+
+            ServicePartitionClient<HttpCommunicationClient> servicePartitionClient =
+                new ServicePartitionClient<HttpCommunicationClient>(
+                    this.clientFactory,
+                    serviceUri,
+                    partitionKey);
+
+            var result = await servicePartitionClient.InvokeWithRetryAsync(
+                client => {
+                    Uri serviceAddress = new Uri(client.BaseAddress, route);
+                    HttpWebRequest request = WebRequest.CreateHttp(serviceAddress);
+                    request.Method = "POST";
+                    request.ContentType = "application/json";
+                    request.Timeout = (int)client.OperationTimeout.TotalMilliseconds;
+                    request.ReadWriteTimeout = (int)client.ReadWriteTimeout.TotalMilliseconds;
+                    request.ContentLength = 0;
+
+                    try {
+                        using (Stream requestStream = request.GetRequestStream()) {
+                            using (BufferedStream buffer = new BufferedStream(requestStream)) {
+                                using (StreamWriter writer = new StreamWriter(buffer)) {
+                                    JsonSerializer serializer = new JsonSerializer();
+                                    serializer.Serialize(writer, payload);
+                                    buffer.Flush();
+                                }
+                            }
+                        }
+
+                        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+                            return Task.FromResult(true);
+                        }
+                    } catch (WebException ex) {
+                        string message;
+                        if (ex.Response != null) {
+                            var responseStream = ex.Response.GetResponseStream();
+                            responseStream.Seek(0, SeekOrigin.Begin);
+                            using (StreamReader sr = new StreamReader(responseStream)) {
+                                message = sr.ReadToEnd();
+                            }
+                        } else { message = "<no message>"; }
+
+                        ServiceEventSource.Current.Message($"EXCEPTION: WebException {ex.Status} caught when trying to POST to {serviceAddress}{Environment.NewLine}{message}");
+                        return Task.FromResult(false);
+                    }
+
+                }, CancellationToken.None);
+
+            return result;
         }
 
         private IBookActor GetActor(string isbn) {
